6de257ab5f2c652e3353d57a7fc3d86c99e34d50
Bilostenko <bilostenkod@gmail.com>
2025-02-28 16:36:39 +0200

    create react-app typescript

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + TypeScript + Vite
+
+стиля на препроцесорі SASS
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.sass'
+const App: React.FC = () => {
+
+  return (
+    <div className="App">
+      <div className="heading">Taskify</div>
+    </div>
+  );
+}
+
+export default App
