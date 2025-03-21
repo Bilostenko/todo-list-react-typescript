730881c9b45a45b7b94459a6cda5be02ca71dc14
BILOSTENKO_D <bilostenkod@gmail.com>
2025-03-21 23:20:50 +0200

    Removed compiled JS files from repo

--- src/app/hooks.js
+++
@@ -1,4 +0,0 @@
-import { useDispatch, useSelector } from 'react-redux';
-// Use throughout the application instead of the usual `useDispatch` and `useSelector`
-export const useAppDispatch = useDispatch;
-export const useAppSelector = useSelector;
--- src/app/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import todoReducer from '../features/submit_form/submitSlice';
-export const store = configureStore({
-    reducer: {
-        todos: todoReducer,
-    },
-});
